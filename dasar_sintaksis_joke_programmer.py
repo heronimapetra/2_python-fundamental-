d1fd76f1266273fd016b9a1422dd880451248e16
rinigadjo <rinigadjo@gmail.com>
2023-01-30 13:07:14 +0800

    Sequential and branching

---
+++ dasar_sintaksis_joke_programmer.py
@@ -0,0 +1,16 @@
+# Percabangan
+
+milk_bottle_count = 173
+egg_count = 1534
+
+if milk_bottle_count > 0:
+    print("Budi mengecek harganya dan ternyata uangnya cukup")
+    if egg_count == 0:
+        print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang ke rumah")
+print("Budi menyampaikan hasil kepada ibunya")
+
+
